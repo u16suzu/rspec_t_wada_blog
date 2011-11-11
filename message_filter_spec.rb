e3ac8508366b48774602011a3ced4daa63e85120
u16suzu <u16suzu@gmail.com>
2011-11-12 01:45:32 +0900

    tdd

---
+++ message_filter_spec.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'rspec'
+require './message_filter'
+
+describe MessageFilter, 'with argument "foo"' do
+  subject { MessageFilter.new('foo') }
+  it { should be_detect('hello from foo') }
+  it { should_not be_detect('hello from bar') }
+end
